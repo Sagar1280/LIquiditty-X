ee26aad24ff2ed7c9dcdc174f0428328d4020801
Sagar M <sagarmurali2004@gmail.com>
2026-01-13 23:09:31 +0530

    added lot of pairs in array and added logo , changed scroll bar

--- Frontend/src/data/cryptoPrices.jsx
+++ Frontend/src/data/cryptoPrices.jsx
@@ -1,10 +1,47 @@
 // cryptoPairs.js
 export const cryptoPairs = [
-    { symbol: "BTC/USDT", price: 43210, change: 2.3 },
+    { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
 
-    { symbol: "ETH/USDT", price: 2320, change: -1.1 },
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
 
-    { symbol: "SOL/USDT", price: 98.45, change: 5.8 },
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+      { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+
+      { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+        { symbol: "BTC/USDT", price: 43210, change: 2.3 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+      { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+      { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },  
+      { symbol: "BTC/USDT", price: 43210, change: 2.3  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+      { symbol: "BTC/USDT", price: 43210, change: 2.3 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
+
+    { symbol: "ETH/USDT", price: 2320, change: -1.1 , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040" },
+
+    { symbol: "SOL/USDT", price: 98.45, change: 5.8  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"},
     
-    { symbol: "SPX/USDT", price: 1.3, change: 10.9 }
+    { symbol: "SPX/USDT", price: 1.3, change: 10.9  , logo :"https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"}
 ];
--- Frontend/src/pages/spot/TradeSpot.jsx
+++ Frontend/src/pages/spot/TradeSpot.jsx
@@ -1,55 +1,50 @@
 import TradingViewWidget from "../../components/TradingViewWidget";
 import { cryptoPairs } from "../../data/cryptoPrices";
 
-
 const TradeSpot = () => {
   return (
     <div className="tradespot">
 
+
       <div className="sidebar">
-        
-        {/* Header */}
+
         <div className="sidebar-header">
-          <div>Crypto</div>
-          <div>Price</div>
-          <div>24h</div>
+          <span>Crypto</span>
+          <span>Price</span>
+          <span>24h</span>
         </div>
 
-        {/* Crypto pairs list */}
+        {/* Crypto list */}
         <div className="crypto_pairs">
-
           {cryptoPairs.map((pair, index) => (
-
             <div className="pair-row" key={index}>
 
-                 <div>{pair.symbol}</div>
+              <div className="pair-info">
+                <img className="pair-logo" src={pair.logo} alt={pair.symbol} />
+                <span>{pair.symbol}</span>
+              </div>
 
-                 <div>${pair.price}</div>
+              <div className="pair-price">${pair.price}</div>
 
-                 <div className={pair.change >= 0 ? "pos" : "neg"}>
-                 {pair.change > 0 ? "+" : ""}
-                 {pair.change}%
-                 </div>
-
+              <div
+                className={`pair-change ${pair.change >= 0 ? "pos" : "neg"}`}
+              >
+                {pair.change > 0 && "+"}{pair.change}%
+              </div>
             </div>
-          )
-          )
-          }
-
+          ))}
         </div>
-
 
       </div>
 
+      {/* MAIN PANEL */}
       <div className="main">
 
         <div className="chart">
           <TradingViewWidget />
         </div>
 
-        <div className="TransactionSpotBar">
-
-        </div>
+        <div className="TransactionSpotBar"></div>
 
       </div>
 
